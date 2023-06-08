7a9d249884b4175e3f7aa8c8e69953d7cca72577
mhimranhossain <mh.imranhossain@outlook.com>
2023-06-08 13:59:23 +0600

    update

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,10 +1,10 @@
 <script>
-  import myImg from "../assets/img.jpg"
+  import myImg from "../assets/img-small.webp"
 </script>
 
 <div class="h-screen flex-grow flex flex-col justify-center items-center space-y-20 sm:space-y-16 lg:space-y-28">
   <div>
-    <img class="rounded-full duration-100 w-44 h-44 sm:w-52 sm:h-52 lg:w-96 lg:h-96"  src={myImg} alt="" />
+    <img loading="lazy" class="rounded-full duration-100 w-44 h-44 sm:w-52 sm:h-52 lg:w-96 lg:h-96"  src={myImg} alt="" decoding="async" />
   </div>
   <button class="bg-purple-600 text-white">See My Work</button>
 </div>
