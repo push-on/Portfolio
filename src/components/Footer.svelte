5d3adcf46d6ba3d377d82eaca639ddcb66b834a4
mhimranhossain <mh.imranhossain@outlook.com>
2023-10-23 21:19:08 +0600

    Update 10/23/2023 21:19:08

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer
   class="mt-20 h-64 w-full bg-black/10 backdrop-blur-3xl text-white flex items-center justify-center border-t-2 border-white/30">
   <div class="text-center">
-    <p>&copy; {year} My Portfolio. All rights reserved.</p>
+    <p>&copy; {year} Pushons Portfolio. All rights reserved.</p>
     <p>Created with Tailwind CSS and Svelte</p>
   </div>
 </footer>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,31 +3,31 @@
 </script>
 
 <div class=" flex justify-center items-center text-white py-20">
-  <div class="w-full">
+  <div class="w-full space-y-24">
     <div class="flex justify-evenly flex-grow">
       <div class=" flex-1">
-        <div class="px-6 sm:px-20 py-20">
+        <div class="px-6 sm:px-10 md:px-15 lg:px-20">
           <div class="overflow-hidden py-4">
             <h2
-              class="SlideDown text-amber-300 whitespace-nowrap duration-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+              class="SlideDown tracking-wide text-amber-400 whitespace-nowrap duration-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
               Hi There, I'm
             </h2>
           </div>
           <div class="overflow-hidden py-4">
             <h1
-              class="SlideDown duration-100 text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl font-bold">
+              class="text-violet-100 SlideDown duration-100 text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-[9rem] font-bold">
               Pushon
             </h1>
           </div>
           <div class="overflow-hidden py-4">
             <h2
-              class="SlideDown text-amber-300 whitespace-nowrap duration-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+              class="SlideDown tracking-wide text-amber-400 whitespace-nowrap duration-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
               Full Stack Devloper
             </h2>
           </div>
           <div class="overflow-hidden py-4">
             <p
-              class="SlideDown max-w-[200px] sm:max-w-xs duration-100 text-sm sm:text-base lg:text-lg lg:font-medium">
+              class=" text-violet-200 tracking-widest SlideDown max-w-[200px] sm:max-w-md duration-100 text-sm sm:text-base lg:text-lg lg:font-medium">
               I am passionate about solving business problems through
               human-centered design. Curious by nature and business-minded.
             </p>
@@ -39,11 +39,11 @@
       </div>
     </div>
     <div class="flex justify-evenly">
-      <div class=" flex-1 flex justify-center items-center">
-        <button>Reach Out To Me</button>
-      </div>
       <div class="flex-1 flex justify-center items-center">
         <button>See My Work</button>
+      </div>
+      <div class=" flex-1 flex justify-center items-center">
+        <button>Reach Out To Me</button>
       </div>
     </div>
   </div>
