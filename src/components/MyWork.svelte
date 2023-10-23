5a631ef5be9ad6640c563b67cb84a52cadfe8004
mhimranhossain <mh.imranhossain@outlook.com>
2023-10-23 21:50:52 +0600

    Update 10/23/2023 21:50:52

--- src/components/MyWork.svelte
+++ src/components/MyWork.svelte
@@ -1,25 +1,93 @@
+<script>
+  const myWork = [
+    {
+      title: "Project 1",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+      techStack: "React, Node.js, MongoDB",
+      imageSrc: "https://placehold.co/600x400/000000/FFFFFF/png",
+    },
+    {
+      title: "Project 2",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+      techStack: "Angular, Express, PostgreSQL",
+      imageSrc: "https://placehold.co/600x400/000000/FFFFFF/png",
+    },
+    {
+      title: "Project 3",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+      techStack: "Vue.js, Ruby on Rails, MySQL",
+      imageSrc: "https://placehold.co/600x400/000000/FFFFFF/png",
+    },
+  ]
+</script>
 
-
-
-
-<div class="flex justify-center items-center text-white">
-  <div class="w-full">
-    <div class="flex justify-evenly flex-grow">
-      <div class=" flex-1">
-        <div class=" p-20">
-          <h1 class="text-8xl font-bold">My Work</h1>
+<div>
+  <div
+    class="overflow-hidden py-4 px-6 sm:px-10 md:px-15 lg:px-20 ">
+    <h1
+      class="text-violet-100 SlideDown duration-100 text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-[9rem] font-bold">
+      My Work
+    </h1>
+  </div>
+  {#each myWork as project (project.title)}
+    <div class="flex justify-center items-center text-white py-10">
+      <div class="w-full ">
+        <div class="flex justify-evenly flex-grow">
+          <div class="flex-1">
+            <div class="px-6 sm:px-10 md:px-15 lg:px-20 ">
+              <div class="overflow-hidden py-4">
+                <h2
+                  class="SlideDown tracking-wide whitespace-nowrap duration-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+                  {project.title}
+                </h2>
+              </div>
+              <div class="overflow-hidden py-4">
+                <p
+                  class="text-violet-200 tracking-widest SlideDown max-w-[200px] sm:max-w-md duration-100 text-sm sm:text-base lg:text-lg lg:font-medium">
+                  {project.description}
+                </p>
+              </div>
+              <div class="overflow-hidden py-4">
+                <p
+                  class="text-violet-200 tracking-widest SlideDown max-w-[200px] sm:max-w-md duration-100 text-sm sm:text-base lg:text-lg lg:font-medium">
+                  Tech Stack: {project.techStack}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-1 flex justify-center items-center ">
+            <img class="rounded-3xl" src={project.imageSrc} alt="" />
+          </div>
+        </div>
+        <div class="flex justify-evenly py-5">
+          <div class="flex-1 flex justify-center items-center">
+            <button>View Work</button>
+          </div>
+          <div class="flex-1 flex justify-center items-center">
+            <button>See Live Demo</button>
+          </div>
         </div>
       </div>
-      <div class="flex-1 flex justify-center items-center">
-      </div>
     </div>
-    <div class="flex justify-evenly">
-      <div class=" flex-1 flex justify-center items-center">
-        <button>Reach Out To Me</button>
-      </div>
-      <div class="flex-1 flex justify-center items-center">
-        <button>See My Work</button>
-      </div>
-    </div>
-  </div>
+  {/each}
 </div>
+
+<style>
+  .SlideDown {
+    animation-delay: 300ms;
+    transform: translateY(-150%);
+    animation-name: slideDown;
+    animation-duration: 1.5s;
+    animation-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes slideDown {
+    to {
+      transform: translate(0);
+    }
+  }
+</style>
